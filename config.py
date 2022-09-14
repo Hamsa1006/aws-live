d2401e8759aba7ad64adda04e040f4be1c7765a5
Hamsa1006 <113083484+Hamsa1006@users.noreply.github.com>
2022-09-14 11:55:51 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.clbcqbtylbe0.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "hamsa-employee"
+customregion = "us-east-1"
